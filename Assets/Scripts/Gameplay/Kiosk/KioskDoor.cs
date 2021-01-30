88a5255100cf118133ad7d3b77a2aeaa37ebc5a4
R. Lowe <r_m_lowe@outlook.com>
2021-01-30 03:20:33 +0000

    stuff didn't commit the first time
    smh

---
+++ Assets/Scripts/Gameplay/Kiosk/KioskDoor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Gameplay.Kiosk
+{
+    public class KioskDoor : MonoBehaviour
+    {
+        private GameController gc;
+
+        private void Awake()
+        {
+            gc = GameObject.FindObjectOfType<GameController>();
+        }
+        
+        public void OnTriggerEnter(Collider other)
+        {
+            Debug.Log("hit!");
+            if (other.tag.Equals("Player"))
+            {
+                gc.ThePlayerGotLost();
+            }
+        }
+    }
+}
