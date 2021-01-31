fc5fa4321f854d355e5d2493bcbb73405ec66e68
R. Lowe <r_m_lowe@outlook.com>
2021-01-31 03:54:17 +0000

    and that didn't commit
    smh

---
+++ Assets/Scripts/TitleScreen/TitleScreenCameraScript.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace TitleScreen
+{
+    public class TitleScreenCameraScript : MonoBehaviour
+    {
+        public float moveSpeed;
+
+        public float moveRange;
+
+        public void Awake()
+        {
+            
+        }
+
+        public void Update()
+        {
+            Vector3 current = transform.position;
+            float currentZ = current.z;
+            
+            currentZ += (moveSpeed * Time.deltaTime);
+
+            if (currentZ > moveRange)
+            {
+                currentZ -= (2 * moveRange);
+            }
+
+
+            transform.position = new Vector3(current.x, current.y, currentZ);
+        }
+    }
+}
